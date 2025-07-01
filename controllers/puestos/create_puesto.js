367f455ffa22dab1fc826484a16e1a17692c71ce
denisegonzalez24 <nissgonzalezm@gmail.com>
2025-07-01 16:58:18 -0300

    feat: Add validation to prevent duplicate puestos and ensure trimmed, lowercase input

--- controllers/puestos/create_puesto.js
+++ controllers/puestos/create_puesto.js
@@ -4,6 +4,20 @@ import PuestoUsuario from '../../models/puesto_usuario.js';
 
 export async function createPuesto(nombre) {
     try {
+        //verificar si ya existe puestos
+        const [{ count }] = await executeQuery(
+            `SELECT COUNT(*) AS count FROM puestos WHERE nombre = ?`,
+            [nombre].trim().toLowerCase(),
+            true, 0
+            );
+        if (count > 0) {
+            throw new CustomException({
+            title:   'Puesto duplicado',
+            message: `Ya existe un tipo_usuario con nombre "${nombre}"`,
+            status:  400
+        });
+        }
+
         const query = `INSERT INTO puestos (nombre) VALUES (?) RETURNING *`;
         const rows = await executeQuery(query, [nombre]);
         return PuestoUsuario.fromJson(rows[0]);
