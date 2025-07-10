69e4583a9b0510f71bcc0d4cf33ecc836afccbcf
denisegonzalez24 <nissgonzalezm@gmail.com>
2025-07-10 11:20:15 -0300

    feat: Add puestoId parameter to createUsuario function; improve error handling and code formatting

--- controllers/usuarios/create_usuario.js
+++ controllers/usuarios/create_usuario.js
@@ -9,44 +9,45 @@ import { hash256 } from '../../src/funciones/hash.js';
  * @param {Object} data - nombre, url_imagen, tipo_usuario_id, email, etc.
  * @returns {Usuario}
  */
-export async function createUsuario(nombre,email,password,urlImagen,tipoUsuarioId) {
+export async function createUsuario(nombre, email, password, urlImagen, tipoUsuarioId, puestoId) {
     try {
 
         const cleanedEmail = email.trim().toLowerCase();
         const [{ count }] = await executeQuery(
-                `SELECT COUNT(*) AS count FROM usuarios WHERE email = ?`, [ cleanedEmail ],
-                true, 0
-            );
-            if (count > 0) {
-                throw new CustomException({
-                title:   'usuario duplicado',
+            `SELECT COUNT(*) AS count FROM usuarios WHERE email = ?`, [cleanedEmail],
+            true, 0
+        );
+        if (count > 0) {
+            throw new CustomException({
+                title: 'usuario duplicado',
                 message: `Ya existe un usuario con nombre"${nombre}" o email "${email}"`,
-                status:  400
+                status: 400
             });
-            }
-        
+        }
+
         const contra_hash = hash256(password);
         const query = `INSERT INTO usuarios (nombre, email, password, url_imagen, tipo_usuario_id) VALUES (?,?,?,?,?)`;
-        const result = await executeQuery(query, [ nombre, cleanedEmail, contra_hash, urlImagen, tipoUsuarioId ],true);
+        const result = await executeQuery(query, [nombre, cleanedEmail, contra_hash, urlImagen, tipoUsuarioId], true);
         //select de la fila recien creada
         const idNuevo = result.insertId;
-        const rows = await executeQuery(
-            `SELECT * FROM usuarios WHERE id = ?`,
-            [idNuevo],
-            true,
-            0
-            ); 
-            
-            if (!rows.length) {
-                throw new Error(`No se encontró usuario con id ${idNuevo}`);
-            }
+        const rows = await executeQuery(`SELECT * FROM usuarios WHERE id = ?`, [idNuevo], true, 0);
+        if (!rows.length) {
+            throw new Error(`No se encontró usuario con id ${idNuevo}`);
+        }
+
+
+        //insertar puesto en puesto_usuario
+        await executeQuery(
+            `INSERT INTO puesto_usuario (usuario_id, puesto_id) VALUES (?, ?)`,
+            [idNuevo, puestoId]
+        );
 
         return Usuario.fromJson(rows[0]);
     } catch (error) {
         throw new CustomException({
-           title: 'Error creating usuario',
-        message:   error.message,
-          stack:  error.stack
+            title: 'Error creating usuario',
+            message: error.message,
+            stack: error.stack
         }
         );
     }
