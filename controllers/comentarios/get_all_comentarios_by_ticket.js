7576621f45a2d668e8678ebfe41ac3235f5052e3
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-07-17 12:04:51 -0300

    refactor: Remove deprecated comentario and menu_modulo functions; add new implementations for ticket comments and menu assignments

--- controllers/comentarios/get_all_comentarios_by_ticket.js
+++ controllers/comentarios/get_all_comentarios_by_ticket.js
@@ -1,17 +1,17 @@
 import { executeQuery } from "../../db.js";
 import CustomException from "../../models/custom_exception.js";
 import Comentario from "../../models/comentario.js";
-
 import { Status } from "../../models/status.js";
 
-export async function getAllComentarios() {
-    const rows = await executeQuery(`SELECT * FROM comentarios WHERE eliminado = 0`
+export async function getAllComentariosByTsicket(req) {
+    const { ticket_id } = req.params;
+    const rows = await executeQuery(`SELECT * FROM comentarios WHERE eliminado = 0 AND ticket_id = ?`, [ticket_id]
     );
 
     if (rows.length === 0) {
         throw new CustomException({
             title: 'No hay comentarios',
-            message: 'No se encontraron comentarios activos',
+            message: `No se encontraron comentarios para el ticket con id: ${ticket_id}`,
             status: Status.noContent
         });
     }
