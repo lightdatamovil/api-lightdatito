e6ce585f59966bd3d4aa7456e1c55489fd8fcf8d
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-07-17 12:07:28 -0300

    refactor: Remove deprecated comentario and menu_modulo functions; add new implementations for ticket comments and menu assignments

--- controllers/menu/menu_modulo/get_all_menu_modulo.js
+++
@@ -1,7 +0,0 @@
-import { executeQuery } from '../../../db.js';
-
-export async function getAllMenuModulo() {
-    // Devuelve todos los módulos activos (no eliminados)
-    return await executeQuery(`SELECT id, nombre, menu_id, fecha_creacion FROM modulos  WHERE eliminado = 0`
-    );
-}
