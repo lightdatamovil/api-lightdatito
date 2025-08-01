85e3cde86806ebe21b1fbce2e0baf197ab1940fe
denisegonzalez24 <nissgonzalezm@gmail.com>
2025-08-01 17:53:07 -0300

    refactor: Organize middleware and route initialization; improve logging and error handling

--- api-lightdatito.js
+++ api-lightdatito.js
@@ -4,12 +4,12 @@ import cors from 'cors';
 import allRoutes from './routes/exports.js';
 import { Status } from './models/status.js';
 import { performance } from 'perf_hooks';
-
 import 'dotenv/config';
 
 const PORT = 13000;
-
 const app = express();
+
+// Middlewares
 app.use(json({ limit: '50mb' }));
 app.use(urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
@@ -17,23 +17,31 @@ app.use(cors({
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+// Ruta de prueba
 app.post('/api/testapi', async (req, res) => {
     const startTime = performance.now();
     const endTime = performance.now();
-    logPurple(`Tiempo de ejecución: ${endTime - startTime} ms`)
+    logPurple(`Tiempo de ejecución: ${endTime - startTime} ms`);
     res.status(Status.ok).json({ message: 'API funcionando correctamente' });
 });
 
-(async () => {
-    try {
-        app.use('/api', allRoutes);
-        app.listen(PORT, () => {
-            logBlue(`Worker ${process.pid} escuchando en el puerto ${PORT}`);
-        });
-    } catch (err) {
-        logRed('Error al iniciar el servidor:', err);
-    }
-})();
+// Cargar rutas principales
+try {
+    app.use('/api', allRoutes);
+} catch (err) {
+    logRed('❌ Error cargando rutas:', err);
+}
 
+// Iniciar servidor
+app.listen(PORT, () => {
+    logBlue(`🟢 Worker ${process.pid} escuchando en el puerto ${PORT}`);
+});
 
-// todo cambiar la tabla paises con un update por lta de logistica en un pais
+// Captura de errores silenciosos
+process.on('unhandledRejection', reason => {
+    console.error('❌ unhandledRejection:', reason);
+});
+process.on('uncaughtException', err => {
+    console.error('❌ uncaughtException:', err);
+});
